fdd18ed7f4de79bed9596e9956d6bae80f69e647
cordellr <cordell.rainock@gmail.com>
2019-01-22 12:54:03 -0600

    Places rectangle on screen with user inputted values and random location

--- main.js
+++ main.js
@@ -1,7 +1,76 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    class Shape {
+        constructor() {
+            this.div = $("<div></div>");
+            $("#draw-box").append(this.div);
+
+            //$(this.div).click(des)
+        }
+
+        divPosition() {
+            this.x = Math.floor(Math.random() * (601 - this.width));
+            this.y = Math.floor(Math.random() * (601 - this.height));
+        }
+    }
+
+    class Rectangle extends Shape {
+        constructor(height, width) {
+            super();
+            $(this.div).addClass("rectangleDiv");
+
+            //describing this.width and this.height is critical to calculating divPosition and populating describe().
+            this.width = width;
+            this.height = height;
+            this.divPosition();
+
+            //important for populating describe().
+            this.name = "Rectangle";
+            this.radius = "NA"
+            this.area = width * height;
+            this.perimeter = 2 * width + 2 * height;
 
 
+            //set css properties for rectangle according to user input and divPosition random values
+            $(this.div).css({
+                "height": height,
+                "width": width,
+                "left": this.x,
+                "top": this.y
+            });
+        }
+    }
 
-    
+    class Square extends Shape {
+        constructor(sideLength) {
+            super();
+            $(this.div).addClass("squareDiv");
+        }
+    }
+
+    class Circle extends Shape {
+        constructor(radius) {
+            super();
+            $(this.div).addClass("circleDiv");
+        }
+    }
+
+    class Triangle extends Shape {
+        constructor(height) {
+            super();
+            $(this.div).addClass("triangleDiv");
+        }
+    }
+
+    $("#rectangle-button").click(function () {
+        rectHeight = $("#rectangle-height").val();
+        rectWidth = $("#rectangle-width").val();
+
+        if ((rectHeight < 600) && (rectWidth < 600)) {
+            let rect = new Rectangle(rectHeight, rectWidth);
+        } else {
+            alert("Enter values less than 600");
+        }
+    })
 
 });
